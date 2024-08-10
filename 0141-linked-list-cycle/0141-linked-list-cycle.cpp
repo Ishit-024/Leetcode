fbf8ff82c1a51ba4c9ee3351aab3636e3193d1bb
Ishit-024 <155139839+Ishit-024@users.noreply.github.com>
2024-08-10 08:00:46 +0530

    Time: 11 ms (44.61%), Space: 15.2 MB (6.51%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,22 +9,18 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
+        ListNode*temp=head;
         if (head == NULL || head->next == NULL) {
             return false; // An empty list or a list with only one node cannot have a cycle
         }
-        
-        ListNode *slow = head;
-        ListNode *fast = head;
-        
-        while (fast != NULL && fast->next != NULL) {
-            slow = slow->next;           // Move slow pointer by 1 step
-            fast = fast->next->next;     // Move fast pointer by 2 steps
-            
-            if (slow == fast) {
-                return true; // Cycle detected
+        unordered_map<ListNode*,bool>mp(0);
+        while(temp!=NULL){
+            if(mp.find(temp)!=mp.end()){
+                return true;
             }
+            mp[temp]=true;
+            temp=temp->next;
         }
-        
-        return false; // No cycle detected
+        return false;
     }
 };
