fb0b76a5f8b3f430cc9271404fc613e6dfd89342
Ishit-024 <155139839+Ishit-024@users.noreply.github.com>
2024-08-13 19:26:15 +0530

    Time: 10 ms (29.43%), Space: 10.4 MB (27.43%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+ListNode* findLoop(ListNode* &head){
+ ListNode*slow=head;
+ ListNode*fast=head;
+        while(slow!=NULL && fast!=NULL){
+            fast=fast->next;
+            if(fast!=NULL){
+                fast=fast->next;
+            }
+            slow=slow->next;
+            if(fast==slow){
+                return slow;
+            }
+        }
+        return NULL;
+    }
+    ListNode *detectCycle(ListNode *head) {
+       if(head==NULL || head->next==NULL){
+        return NULL;
+       }
+       else if(head->next==head){
+        return head;
+       }
+       else if(findLoop(head)==NULL){
+        return NULL;
+       }
+       ListNode* x=findLoop(head); //gets slow pointer or fast pointer
+       ListNode* y=head;
+       while(x!=y){
+        x=x->next;
+        y=y->next;
+       }
+       return x;
+    }
+};
