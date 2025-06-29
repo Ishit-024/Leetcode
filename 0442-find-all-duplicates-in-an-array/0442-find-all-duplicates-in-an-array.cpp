82bae7ae4e05f1277b20af3ae93414f47c13108b
Ishit Setia <155139839+Ishit-024@users.noreply.github.com>
2025-06-29 10:44:33 +0530

    Time: 15 ms (42.72%), Space: 48.6 MB (38.49%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -2,14 +2,14 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-    vector<int>v;
-    int count=0;
-    sort(nums.begin(),nums.end());
+    vector<int>ans;
+    std::sort(nums.begin(),nums.end());
     for(int i=0;i<nums.size()-1;i++){
-        if(nums[i+1]==nums[i]){
-            v.push_back(nums[i]);
+        if(nums[i]==nums[i+1]){
+            ans.push_back(nums[i]);
+            i++;
         }
     }
-    return v;
+    return ans;
     }
     };
