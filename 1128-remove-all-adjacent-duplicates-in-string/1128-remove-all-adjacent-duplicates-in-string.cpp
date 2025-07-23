a3864639542e67ee59ef90f98bc36e5377bf4a91
Ishit Setia <155139839+Ishit-024@users.noreply.github.com>
2025-07-24 03:03:18 +0530

    Time: 39 ms (19.52%), Space: 12.6 MB (94.59%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -3,7 +3,7 @@ public:
     string removeDuplicates(string s) {
         for (int i = 0; i < (int)s.length() - 1; i++) {
             if (s[i] == s[i + 1]) {
-                s.erase(i, 2);  // erase 2 characters starting at index i
+                s.erase(s.begin()+i,s.begin()+i+2);  // erase 2 characters starting at index i
                 i = max(-1, i - 2);  // step back to check for new duplicates
             }
         }
