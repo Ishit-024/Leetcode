e656bbbdb02bce07e49649aa58ff8efddda0e8ee
Ishit Setia <155139839+Ishit-024@users.noreply.github.com>
2025-07-23 08:25:30 +0530

    Time: 0 ms (100%), Space: 14.2 MB (58.6%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,27 +1,43 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-       vector<int>v;
-       vector<int>ans;
-       int i=0;
-       int j=0;
        std::sort(nums1.begin(),nums1.end());
        std::sort(nums2.begin(),nums2.end());
+       vector<int>ans;
+       set<int>s;
+       int i=0;
+       int j=0;
        while(i<nums1.size() && j<nums2.size()){
-        if(nums1[i]<nums2[j]){
+        if(nums1[i]==nums2[j]){
+            ans.push_back(nums1[i]);
             i++;
-        }
-        else if(nums1[i]>nums2[j]){
             j++;
         }
+        else if(nums1[i]<nums2[j]){
+            i++;
+        }
         else{
-            if (ans.empty() || ans.back() != nums1[i]) {
-                    ans.push_back(nums1[i]);
-                }
-                i++;
-                j++;
+            j++;
+        }
        }
+       int x=i;
+    //    if(i<nums1.size()){
+    //     for(int k=i;k<nums1.size();k++){
+    //         ans.push_back(nums1[k]);
+    //     }
+    //    }
+    //    else if(j<nums2.size()){
+    //     for(int t=j;t<nums2.size();t++){
+    //         ans.push_back(nums2[t]);
+    //     }
+    //    }
+       for(auto i:ans){
+        s.insert(i);
        }
-       return ans;
+       vector<int>v;
+       for(auto i:s){
+        v.push_back(i);
        }
+       return v;
+    }
 };
