9a58d1036e1d8639d54b57d16428a1261e73e2f9
Ishit Setia <155139839+Ishit-024@users.noreply.github.com>
2025-07-23 21:05:50 +0530

    Time: 0 ms (100%), Space: 10.3 MB (18.19%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-string toLowerCase(string c){
-    for(int i=0;i<c.length();i++){
-    if(c[i]>='A' && c[i]<='Z'){
-        c[i]=c[i]-'A'+'a';
-    }
-    }
-    return c;
-}
+
  bool valid(char c){
-            if(c>='a' && c<='z' || c>='A' && c<='Z' || c>='0' && c<='9'){
-                return true;
-            }
+    if(c>='a' && c<='z' || c>='A' && c<='Z' || c>='0' && c<='9'){
+        return true;
+    }
+    return false;
+ }
+ char tolower(char c){
+    if(c>='a' && c<='z'){
+        return c;
+    }
+    char t=c-'A'+'a';
+    return t;
+ }
+    bool isPalindrome(string s) {
+       string x;
+       for(int i=0;i<s.length();i++){
+        if(valid(s[i])){
+            x.push_back(s[i]);
+        }
+       }
+       for(int i=0;i<x.length();i++){
+        x[i]=tolower(x[i]);
+       }
+       for(int i=0;i<x.length()/2;i++){
+        if(x[i]!=x[x.length()-i-1]){
             return false;
         }
-    bool isPalindrome(string s) {
-        string ans;
-        for(int i=0;i<s.length();i++){
-            if(valid(s[i])){
-                ans.push_back(s[i]);
-            }
-        }
-       string ans2=toLowerCase(ans);
-        string ans1=ans2;
-        for(int i=0;i<ans2.length()/2;i++){
-            swap(ans2[i],ans2[ans.length()-i-1]);
-        }
-        return ans2==ans1;
+       }
+       return true;
     }
+
 };
