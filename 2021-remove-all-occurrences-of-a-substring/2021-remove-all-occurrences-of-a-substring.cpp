612a9d4e3aa34a7d9088c48ca3c9bc21a27f1ec2
Ishit Setia <155139839+Ishit-024@users.noreply.github.com>
2025-07-24 00:10:03 +0530

    Time: 0 ms (100%), Space: 9.4 MB (38.34%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        while(s.length()!=0 && s.find(part)<s.length()){
-            s.erase(s.begin()+s.find(part),s.begin()+s.find(part)+part.length());
-        }
-        return s;
+      while(s.length()!=0 && s.find(part)<s.length()){
+        s.erase(s.begin()+s.find(part),s.begin()+s.find(part)+part.length());
+      }
+      return s;
     }
 };
