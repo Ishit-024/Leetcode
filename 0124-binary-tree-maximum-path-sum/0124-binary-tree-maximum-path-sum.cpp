ff4f1c876317a6d878cae870db243b8699316236
Ishit Setia <155139839+Ishit-024@users.noreply.github.com>
2025-07-26 08:23:53 +0530

    Time: 0 ms (100%), Space: 28 MB (48.38%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -9,7 +9,10 @@ public:
     }
 
     int maxPathSum(TreeNode* root) {
-        solve(root);         // call the helper function
+        solve(root); 
+        if(leaf(root)){
+            return root->val;
+        }        // call the helper function
         return maxi;         // return the final max path sum
     }
 
@@ -17,11 +20,7 @@ public:
         if (root == NULL) {
             return 0;
         }
-        else if (leaf(root)) {
-            maxi = max(maxi, root->val);  // Update maxi in case it's the only node
-            return root->val;
-        }
-
+      
         int l = max(0, solve(root->left));
         int r = max(0, solve(root->right));
         maxi = max(maxi, l + r + root->val);
